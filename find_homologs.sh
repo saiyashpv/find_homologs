901d5cf1ae3d08d6fc97ad4ad9f24615a3b1c0df
saiyashpv <saiyashvishnubhatt@yahoo.com>
2024-09-09 21:14:43 -0400

    Adjusted awk args from tblastn output

--- find_homologs.sh
+++ find_homologs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 tblastn -query "$1" -subject "$2" -outfmt '6 pident length qlen' > "$3"
-awk '$1 > 30 && $4 >= (0.9 * $5)' "$3" > "temp.txt"
+awk '$1 > 30 && $2 >= (0.9 * $3)' "$3" > "temp.txt"
 mv temp.txt "$3"
 num_matches=$(wc -l < "$3")
 echo "$num_matches perfect matches found"
