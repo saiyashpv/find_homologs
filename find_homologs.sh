43f534c97a58b4c939c4a114fb713e585c8443b5
saiyashpv <saiyashvishnubhatt@yahoo.com>
2024-09-09 21:02:04 -0400

    Syntax error in tblastn statement

--- find_homologs.sh
+++ find_homologs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-tblastn -query "$1" -subject "$2" -outfmt '6pident length qlen' > "$3"
+tblastn -query "$1" -subject "$2" -outfmt '6 pident length qlen' > "$3"
 awk '$1 > 30 && $4 >= (0.9 * $5)' "$3" > "temp.txt"
 mv temp.txt "$3"
 num_matches=$(wc -l < "$3")
